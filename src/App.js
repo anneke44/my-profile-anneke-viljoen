e8a64915e5812f1eb2b85b55276b92b11fe1186e
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-08 12:55:18 +0200

    Building NavBar Structure

--- src/App.js
+++ src/App.js
@@ -7,43 +7,16 @@ function App() {
     <div className="App">
       <div className="container">
         <section>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="#">
-                <img src={logo} className="logo img img-fluid" />
-              </a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarText"
-                aria-controls="navbarText"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarText">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
-                      About
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Profile
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      Contact
-                    </a>
-                  </li>
+          <nav>
+            <div className="navHeader d-flex">
+              <div className="logo">LogoImg</div>
+              <div>Some Text</div>
+              <div className="dropDown">
+                <ul>
+                  <li>About</li>
+                  <li>Profile</li>
+                  <li>Contact</li>
                 </ul>
-                <span className="navbar-text">
-                  Navbar text with an inline element
-                </span>
               </div>
             </div>
           </nav>
