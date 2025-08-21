b047daa3dcef30e40e628f5a371061cd77b0b2cb
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-21 14:15:28 +0200

    Added a NavBar component to the App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import logo from "./AnnekeLogo.png";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <section>
-          <nav>
-            <div className="navHeader d-flex">
-              <div className="logo">LogoImg</div>
-              <div>Some Text</div>
-              <div className="dropDown">
-                <ul>
-                  <li>About</li>
-                  <li>Profile</li>
-                  <li>Contact</li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </section>
+        <NavBar />
       </div>
     </div>
   );
---
+++ src/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./AnnekeLogo.png";
+
+export default function NavBar() {
+  return (
+    <section>
+      <nav className="NavBar">
+        <div className="nav-header d-flex">
+          <div className="logo">
+            <a href="/" title="homepage">
+              <img
+                src={logo}
+                alt="Logo"
+                className="anneke-logo img img-fluid"
+              />
+            </a>
+          </div>
+          <div>Anneke Viljoen</div>
+          <div className="dropDown">
+            <ul>
+              <li>About</li>
+              <li>Profile</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </section>
+  );
+}
