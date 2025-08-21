f14347a8a5901a3107285eb304d2f51e4b913175
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-21 15:20:34 +0200

    Added HomepageHero component and basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import NavBar from "./NavBar";
+import HomepageHero from "./HomepageHero";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <NavBar />
+        <HomepageHero />
       </div>
     </div>
   );
---
+++ src/HomepageHero.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./HomepageHero.css";
+
+export default function HomepageHero() {
+  return (
+    <div className="HomepageHero d-flex">
+      <div>
+        <div className="primary-heading">
+          <ul>
+            <li>Anneke Viljoen</li>
+            <li>Front End Developer</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <a
+          id="button-link"
+          href="/contact.html"
+          title="Contact Anneke"
+          className="text-decoration-none"
+        >
+          <button id="launch-button" className="btn btn-branding">
+            Contact
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+}
