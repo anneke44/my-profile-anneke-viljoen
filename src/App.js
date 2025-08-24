d7bba2cf6cd897730ee8b10b04509fd627a0502e
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-24 16:35:20 +0200

    Added Roboto font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./NavBar";
 import HomepageHero from "./HomepageHero";
@@ -11,15 +11,13 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route exact path="/" element={<HomepageHero />} />
-            <Route exact path="About" element={<About />} />
-            <Route exact path="Profile" element={<Profile />} />
-            <Route exact path="Contact" element={<Contact />} />
-          </Routes>
-        </Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<HomepageHero />} />
+          <Route path="About" element={<About />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="Contact" element={<Contact />} />
+        </Routes>
       </div>
     </div>
   );
