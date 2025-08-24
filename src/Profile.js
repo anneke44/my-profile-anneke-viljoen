148aafa073c4d47e5bbc2450b08c7b49027847e6
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-24 17:52:38 +0200

    Added basic structure of Portfolio component

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Bg from "./bg.png";
 
 export default function Profile() {
-  return <div className="Profile">"Hallo Profile"</div>;
+  return (
+    <div className="Profile">
+      <div>
+        <h1>My Portfolio</h1>
+      </div>
+      <div className="portfolio-container container">
+        <div className="row g-4">
+          <div className="col-md-6">
+            <img src={Bg} alt="Placeholder" className="img-fluid" />
+          </div>
+          <div className="col-md-6">
+            <img src={Bg} alt="Placeholder" className="img-fluid" />
+          </div>
+          <div className="col-md-6">
+            <img src={Bg} alt="Placeholder" className="img-fluid" />
+          </div>
+          <div className="col-md-6">
+            <img src={Bg} alt="Placeholder" className="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
