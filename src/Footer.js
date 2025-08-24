51b2a8ba0ccd408e234c3e176ae28de2dc3c7d41
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-24 21:02:02 +0200

    Added Footer component

--- src/Footer.js
+++ src/Footer.js
@@ -2,14 +2,21 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
 
 library.add(fab);
 
 export default function Footer() {
   return (
     <div className="Footer">
-      <FontAwesomeIcon icon="fa-brands fa-github" />
-      <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+      Built by Anneke Viljoen. Open sourced on GitHub
+      <a href="https://github.com/anneke44" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon="fa-brands fa-github" />
+      </a>
+      , and hosted on Netlify. Check out my LinkedIn
+      <a href="https://github.com/anneke44" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+      </a>
     </div>
   );
 }
