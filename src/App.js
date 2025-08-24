ff9414b20af0ad34e5c1f9602a70365501afe948
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-24 15:38:28 +0200

    Installed React Router Dom for page navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { Routes, Route, Router } from "react-router-dom";
 import "./App.css";
 import NavBar from "./NavBar";
 import HomepageHero from "./HomepageHero";
+import About from "./About";
+import Profile from "./Profile";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <NavBar />
-        <HomepageHero />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<HomepageHero />} />
+            <Route exact path="About" element={<About />} />
+            <Route exact path="Profile" element={<Profile />} />
+            <Route exact path="Contact" element={<Contact />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   );
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 import logo from "./AnnekeLogo.png";
 
@@ -8,20 +9,26 @@ export default function NavBar() {
       <nav className="NavBar">
         <div className="nav-header d-flex">
           <div className="logo">
-            <a href="/" title="homepage">
+            <Link to="/" title="Homepage">
               <img
                 src={logo}
                 alt="Logo"
                 className="anneke-logo img img-fluid"
               />
-            </a>
+            </Link>
           </div>
           <div>Anneke Viljoen</div>
           <div className="dropDown">
             <ul>
-              <li>About</li>
-              <li>Profile</li>
-              <li>Contact</li>
+              <li>
+                <Link to="./About">About</Link>
+              </li>
+              <li>
+                <Link to="./Profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="./Contact">Contact</Link>
+              </li>
             </ul>
           </div>
         </div>
