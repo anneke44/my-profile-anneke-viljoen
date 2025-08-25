ead31bd5c7dba10f8d4e28d39f888e586d5715b6
Anneke <122925139+anneke44@users.noreply.github.com>
2025-08-25 20:50:16 +0200

    Added images of projects to profile and styles the cards

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Profile.css";
-import Bg from "./bg.png";
+import CapeTown from "./capetown.png";
+import DictionaryApp from "./dictionaryapp.png";
+import WeatherApp from "./weatherapp.png";
+import WorldClock from "./worldclock.png";
+import ShortStory from "./shortstory.png";
 
 export default function Profile() {
   return (
@@ -12,7 +16,7 @@ export default function Profile() {
         <div className="row row-cols-1 row-cols-md-2 g-4">
           <div className="col">
             <div className="card">
-              <img src={Bg} className="card-img-top" alt="..." />
+              <img src={CapeTown} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
@@ -25,7 +29,7 @@ export default function Profile() {
           </div>
           <div className="col">
             <div className="card">
-              <img src={Bg} className="card-img-top" alt="..." />
+              <img src={DictionaryApp} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
@@ -38,7 +42,7 @@ export default function Profile() {
           </div>
           <div className="col">
             <div className="card">
-              <img src={Bg} className="card-img-top" alt="..." />
+              <img src={WeatherApp} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
@@ -50,7 +54,20 @@ export default function Profile() {
           </div>
           <div className="col">
             <div className="card">
-              <img src={Bg} className="card-img-top" alt="..." />
+              <img src={WorldClock} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img src={ShortStory} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
