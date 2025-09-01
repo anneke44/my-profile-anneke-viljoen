aeac83bdef02e99696cd33394d59ad32e5e98d11
Anneke <122925139+anneke44@users.noreply.github.com>
2025-09-01 15:41:19 +0200

    Router fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 import "@fontsource-variable/roboto";
 import "./index.css";
 import App from "./App";
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
